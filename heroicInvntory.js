80f6f33d567ada83b6d40c4268ba9cad01904683
Nikoleta Zlateva <87802876+Nikoletazl@users.noreply.github.com>
2022-05-30 20:21:48 +0300

    added more problems

---
+++ heroicInvntory.js
@@ -0,0 +1,12 @@
+function solve(array){
+    let result = []
+    for (const iterator of array){
+        let [name, level, items] = iterator.split(' / ')
+        level = Number(level)
+        items = items ? items.split(', ') : []
+        result.push({name, level, items});
+
+}  
+    console.log(JSON.stringify(result))
+}
+solve(['Jake / 1000 / Gauss, HolidayGrenade'])
